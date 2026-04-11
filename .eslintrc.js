ab0e82b6506e96babd4d2efa8970c4e1b07fdd0c
John Karahalis <john@johnkarahalis.com>
2026-04-10 20:36:47 -0400

    Uninstall unused TypeScript ESLint packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,11 @@ module.exports = {
     es2021: true,
   },
   extends: ["airbnb-base", "plugin:json/recommended", "prettier"],
-  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint", "json"],
+  plugins: ["json"],
   overrides: [
     // Allow devDependencies to be imported in the .posthtmlrc.js configuration
     // file
